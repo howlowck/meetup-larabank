5df6b2ed5cf9f7a3d9ca023e083b1a8de505d6f3
Hao Luo <howlowck@gmail.com>
2014-02-17 22:32:56 -0600

    initial commit

---
+++ app/LaraBank/Account.php
@@ -0,0 +1,28 @@
+<?php namespace \LaraBank;
+
+class Account {
+	protected $accountNumber;
+	protected $userName;
+	protected $dogecoins;
+
+	public function __construct($userName, $amount)
+	{
+
+	}
+
+	public function amountInDogecoin()
+	{
+
+	}
+
+	public function amountInUSD()
+	{
+
+	}
+
+	public function deposit($amount)
+	{
+
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Laravel Chicago Unit Testing Workshop
+
+You are asked to create a LaraBank.
+
+Download this starter project and follow along :)
+
